b3afdd2120abb25271c6378dca292512e44acae1
Berkay Sergen AK <berkaysergenak@hotmail.com>
2023-10-31 23:45:33 +0300

    Biz kimiz kısmı Api işlemleri

---
+++ RealEstate_Dapper_Api/Dtos/WhoWeAreDetailDtos/UpdateWhoWeAreDetailDto.cs
@@ -0,0 +1,11 @@
+﻿namespace RealEstate_Dapper_Api.Dtos.WhoWeAreDetailDtos
+{
+    public class UpdateWhoWeAreDetailDto
+    {
+        public int WhoWeAreDetaiId { get; set; }
+        public string Title { get; set; }
+        public string Subtitle { get; set; }
+        public string Description { get; set; }
+        public string Description2 { get; set; }
+    }
+}
---
+++ RealEstate_Dapper_Api/Repositories/WhoWeAreDetailRepository/WhoWeAreDetailRepository.cs
@@ -0,0 +1,78 @@
+﻿using Dapper;
+using RealEstate_Dapper_Api.Dtos.WhoWeAreDetailDtos;
+using RealEstate_Dapper_Api.Models.DapperContext;
+
+namespace RealEstate_Dapper_Api.Repositories.WhoWeAreDetailRepository
+{
+    public class WhoWeAreDetailRepository : IWhoWeAreDetailRepository
+    {
+        private readonly Context _context;
+
+        public WhoWeAreDetailRepository(Context context)
+        {
+            _context = context;
+        }
+
+        public async void CreateWhoWeAreDetail(CreateWhoWeAreDetailDto createWhoWeAreDetailDto)
+        {
+            string query = "insert into WhoWeAreDetail (Title, Subtitle, Description, Description2) values (@title, @subtitle, @description, @description2)";
+            var parameters = new DynamicParameters();
+            parameters.Add("@title", createWhoWeAreDetailDto.Title);
+            parameters.Add("@subtitle", createWhoWeAreDetailDto.Subtitle);
+            parameters.Add("@description", createWhoWeAreDetailDto.Description);
+            parameters.Add("@description2", createWhoWeAreDetailDto.Description2);
+            using (var connection = _context.CreateConnection())
+            {
+                await connection.ExecuteAsync(query, parameters);
+            }
+        }
+
+        public async void DeleteWhoWeAreDetail(int id)
+        {
+            string query = "Delete From WhoWeAreDetail Where WhoWeAreDetailId = @whoWeAreDetailId";
+            var parameters = new DynamicParameters();
+            parameters.Add("@whoWeAreDetailId", id);
+            using (var connection = _context.CreateConnection())
+            {
+                await connection.ExecuteAsync(query, parameters);
+            }
+        }
+
+        public async Task<List<ResultWhoWeAreDetailDto>> GetAllWhoWeAreDetailAsync()
+        {
+            string query = "Select * From WhoWeAreDetail";
+            using (var connection = _context.CreateConnection())
+            {
+                var values = await connection.QueryAsync<ResultWhoWeAreDetailDto>(query);
+                return values.ToList();
+            }
+        }
+
+        public async Task<GetByIdWhoWeAreDetailDto> GetWhoWeAreDetail(int id)
+        {
+            string query = "Select * From WhoWeAreDetail Where WhoWeAreDetailId = @whoWeAreDetailId";
+            var parameters = new DynamicParameters();
+            parameters.Add("@whoWeAreDetailId", id);
+            using (var connection = _context.CreateConnection())
+            {
+                var values = await connection.QueryFirstOrDefaultAsync<GetByIdWhoWeAreDetailDto>(query, parameters);
+                return values;
+            }
+        }
+
+        public async void UpdateWhoWeAreDetail(UpdateWhoWeAreDetailDto updateWhoWeAreDetailDto)
+        {
+            string query = "Update WhoWeAreDetail Set Title = @title, Subtitle = @subtitle, Description =@description, Description2 =@description2 where WhoWeAreDetailId = @whoWeAreDetailId";
+            var parameters = new DynamicParameters();
+            parameters.Add("@title", updateWhoWeAreDetailDto.Title);
+            parameters.Add("@subtitle", updateWhoWeAreDetailDto.Subtitle);
+            parameters.Add("@description", updateWhoWeAreDetailDto.Description);
+            parameters.Add("@description2", updateWhoWeAreDetailDto.Description2);
+            parameters.Add("@whoWeAreDetailId", updateWhoWeAreDetailDto.WhoWeAreDetaiId);
+            using (var connection = _context.CreateConnection())
+            {
+                await connection.ExecuteAsync(query, parameters);
+            }
+        }
+    }
+}
